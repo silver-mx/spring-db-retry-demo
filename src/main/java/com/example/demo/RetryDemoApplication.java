46d1a5091721ec0aaa45d1b6c35e28e706bb7819
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2019-11-03 15:24:15 +0100

    Add Spring retry demo

---
+++ src/main/java/com/example/demo/RetryDemoApplication.java
@@ -0,0 +1,71 @@
+package com.example.demo;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.datasource.AbstractDataSource;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Retryable;
+
+@SpringBootApplication
+@EnableRetry
+public class RetryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetryDemoApplication.class, args);
+	}
+
+	@Order(Ordered.HIGHEST_PRECEDENCE)
+	private class RetryableDataSourceBeanPostProcessor implements BeanPostProcessor {
+		@Override
+		public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+			if (bean instanceof DataSource) {
+				bean = new RetryableDataSource((DataSource) bean);
+			}
+			return bean;
+		}
+
+		@Override
+		public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+			return bean;
+		}
+	}
+
+	@Bean
+	public BeanPostProcessor dataSouceWrapper() {
+		return new RetryableDataSourceBeanPostProcessor();
+	}
+
+}
+
+class RetryableDataSource extends AbstractDataSource {
+
+	private DataSource delegate;
+
+	public RetryableDataSource(DataSource delegate) {
+		this.delegate = delegate;
+	}
+
+	@Override
+	@Retryable(maxAttemptsExpression = "#{${db.retries:10}}", backoff = @Backoff(multiplier = 2.3, maxDelayExpression = "#{${db.retry.delay:30000}}"))
+	public Connection getConnection() throws SQLException {
+		return delegate.getConnection();
+	}
+
+	@Override
+	@Retryable(maxAttempts = 10, backoff = @Backoff(multiplier = 2.3, maxDelay = 10000))
+	public Connection getConnection(String username, String password) throws SQLException {
+		return delegate.getConnection(username, password);
+	}
+
+}
